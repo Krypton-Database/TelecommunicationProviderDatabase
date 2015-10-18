870ac1bca7b1d7344bbdc6c07e13231cfb7efc45
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-10-19 00:40:50 +0300

    Created a class and a basic setup for the JSON report generator (JsonReportGenerator).

---
+++ TelecommunicationProvider/TelecommunicationProvider.Data/Generators/JsonReportGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace TelecommunicationProvider.Data.Generators
+    {
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Script.Serialization;
+    using TelecommunicationProvider.Models.SqlServerModels;
+
+    class JsonReportGenerator
+        {
+        public static void generateJson()
+            {
+            var serializer = new JavaScriptSerializer();
+            var package = new Package;
+            
+            int id = package.Id;
+            string name = package.Name;
+            decimal? price = package.Price;
+            ICollection<Contract> contracts = package.Contracts;
+            }
+
+        }
+    }
