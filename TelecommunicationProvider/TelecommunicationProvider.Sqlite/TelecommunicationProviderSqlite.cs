c6a1a5f3bc37e0734769bf2ad9ba778cd5a42ccd
alexizvely <alexizvely@gmail.com>
2015-10-19 15:53:29 +0300

    sql lite

---
+++ TelecommunicationProvider/TelecommunicationProvider.Sqlite/TelecommunicationProviderSqlite.cs
@@ -0,0 +1,18 @@
+﻿namespace TelecommunicationProvider.Sqlite
+{
+    using System;
+    using System.Linq;
+
+    public class TelecommunicationProviderSqlite
+    {
+        public static void Main()
+        {
+            var db = new UserContext();
+
+            db.User.Add(new User { PhoneManufacturer = "Sony", PhoneModel = "Xperia Z" });
+            db.SaveChanges();
+
+            Console.WriteLine(db.User.Count());
+        }
+    }
+}
---
+++ TelecommunicationProvider/TelecommunicationProvider.Sqlite/UserContext.cs
@@ -0,0 +1,15 @@
+﻿namespace TelecommunicationProvider.Sqlite
+{
+    using System.Data.Entity;
+
+    public class UserContext : DbContext
+    {
+        public UserContext()
+            : base("UserContextSqlLiteDb")
+        {
+            this.Database.CreateIfNotExists();
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
