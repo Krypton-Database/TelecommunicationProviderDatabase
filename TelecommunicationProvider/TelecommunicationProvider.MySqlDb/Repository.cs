ebf2d0b2bc20d9d6f354e0e8a1eefdb4c9d86266
Aleksandra Stojcheva <astojceva@yahoo.com>
2015-10-19 17:50:59 +0300

    My sql working now

---
+++ TelecommunicationProvider/TelecommunicationProvider.MySqlDb/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecommunicationProvider.MySqlDb
+{
+    using TelecommunicationProvider.MySqlDb.Models;
+
+    public class Repository
+    {
+        private TelecommunicationProviderMySqlDbContext context;
+
+        public Repository(TelecommunicationProviderMySqlDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(ModelsMySql model)
+        {
+            this.context.Add(model);
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+    }
+}
